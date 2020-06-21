d0762a8710f7c51fc219057d22a42e66a892169e
Fitrhya2926 <66058165+Fitrhya2926@users.noreply.github.com>
2020-06-21 23:54:31 +0800

    Update c.sh

--- c.sh
+++ c.sh
@@ -3,13 +3,13 @@
 u="y"
 while [ $u = "y" ]
 do
-   python 999dice.py -c 0
+   python3 999dice.py -c 0
    sleep 10
-   python 999dice.py -c 0
+   python3 999dice.py -c 0
    sleep 15
-   python 999dice.py -c 0
+   python3 999dice.py -c 0
    sleep 10
-   python 999dice.py -c 0
+   python3 999dice.py -c 0
    sleep 15
    
 done
